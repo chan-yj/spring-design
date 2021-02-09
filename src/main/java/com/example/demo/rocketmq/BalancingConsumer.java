f4e0a3f37064ab8c5df61940d4d9643a19872331
caohr <caohr@szlanyou.com>
2021-02-09 22:35:28 +0800

    rocketmq

---
+++ src/main/java/com/example/demo/rocketmq/BalancingConsumer.java
@@ -0,0 +1,40 @@
+package com.example.demo.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+/**
+ * 负载均衡消费，集群模式消费
+ *
+ */
+public class BalancingConsumer {
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者,指定组名
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("group1");
+        // 指定Namesrv地址信息.
+        consumer.setNamesrvAddr("47.115.6.182:9876");
+        // 订阅Topic，* 代表所有标签
+        consumer.subscribe("Test", "*");
+        //负载均衡模式消费
+        consumer.setMessageModel(MessageModel.CLUSTERING);
+        // 注册回调函数，处理消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n",
+                        Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        //启动消息者
+        consumer.start();
+        System.out.printf("BalancingConsumer Started.%n");
+    }
+}
---
+++ src/main/java/com/example/demo/rocketmq/SyncProducer.java
@@ -0,0 +1,36 @@
+package com.example.demo.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * 发送同步信息
+ * 这种可靠性同步地发送方式使用的比较广泛，比如：重要的消息通知，短信通知。
+ */
+public class SyncProducer {
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("producer1");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr("47.115.6.182:9876");
+        // 启动Producer实例
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            // 创建消息，并指定Topic，Tag和消息体
+            Message msg = new Message("Test",
+                    "TagA",
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            // 发送消息到一个Broker
+            SendResult sendResult = producer.send(msg);
+            // 通过sendResult返回消息是否成功送达
+            System.out.printf("%s%n", sendResult);
+        }
+        // 如果不再发送消息，关闭Producer实例。
+        // 请把代码中producer.shutdown()这句注掉，生产环境中请加上。
+        // producer.shutdown();
+    }
+
+}
